4d8169efd56cea55c1af32931c7d95af51e7fdc0
kvoros <kinga.voros@cae.com>
2019-05-08 15:16:49 +0200

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -11,7 +10,10 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, Routes} from '@angular/router';
 
+
+const appRoutes ="Routes"
 
 @NgModule({
   declarations: [
