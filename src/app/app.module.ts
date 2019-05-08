a8ba680d77007d596d92a64b63f9b3536a6be57d
kvoros <kinga.voros@cae.com>
2019-05-08 15:22:25 +0200

    Completed the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,12 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes} from '@angular/router';
 
 
-const appRoutes ="Routes"
+const appRoutes: Routes = [
+  {path: 'products', component: ProductListComponent },
+  {path: 'product/:id', component: ProductPageComponent },
+  {path: '', redirectTo: 'products', pathMatch: 'full' }
+
+];
 
 @NgModule({
   declarations: [
@@ -21,8 +26,7 @@ const appRoutes ="Routes"
     ProductPageComponent,
     ProductDescriptionComponent,
     ProductTracklistingComponent,
-    ProductListComponent,
-    
+    ProductListComponent    
   ],
   imports: [
     BrowserModule,
